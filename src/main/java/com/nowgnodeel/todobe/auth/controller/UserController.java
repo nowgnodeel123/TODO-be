4c94bcd76b7f9553afa3a845d9047fcc4f3f4c94
nowgnodeel123 <168517856+nowgnodeel123@users.noreply.github.com>
2025-08-06 17:42:14 +0900

    feat: User controller, repository, service 추가

---
+++ src/main/java/com/nowgnodeel/todobe/auth/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.nowgnodeel.todobe.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
